26f9dfd7467cc3d8bb33804d2e29e784e95d235e
Michal Walczynski <mwalczynski@pgs-soft.com>
2017-12-29 10:52:35 +0100

    Fixes

--- RESTApiConsumptionExample/Program.cs
+++ RESTApiConsumptionExample/Program.cs
@@ -5,31 +5,29 @@ namespace RESTApiConsumptionExample
 {
     public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Task.Run(async () =>
+            Console.WriteLine("Waiting for response...");
+
+            var gitHubHttpClient = new GitHubHttpClient();       
+            var httpClientResponse = await gitHubHttpClient.GetListOfUserRepos("mwalczynski");
+            var gitHubRestSharp = new GitHubRestSharp();
+            var restSharpResponse = await gitHubRestSharp.GetListOfUserRepos("mwalczynski");
+
+            Console.WriteLine("HttpClient");
+            foreach (var repo in httpClientResponse)
             {
+                Console.WriteLine($"Name: {repo.Name}, Link: {repo.Url}");
+            }
+            gitHubHttpClient.Dispose();
 
-                Console.WriteLine("HttpClient");
-                var gitHubHttpClient = new GitHubHttpClient();
-                var httpClientResponse = await gitHubHttpClient.GetListOfUserRepos("mwalczynski");
-                foreach (var repo in httpClientResponse)
-                {
-                    Console.WriteLine($"Name: {repo.Name}, Link: {repo.Url}");
-                }
-                gitHubHttpClient.Dispose();
+            Console.WriteLine("RestSharp");      
+            foreach (var repo in restSharpResponse)
+            {
+                Console.WriteLine($"Name: {repo.Name}, Link: {repo.Url}");
+            }
 
-                Console.WriteLine("RestSharp");
-                var gitHubRestSharp = new GitHubRestSharp();
-                var restSharpResponse = await gitHubRestSharp.GetListOfUserRepos("mwalczynski");
-                foreach (var repo in restSharpResponse)
-                {
-                    Console.WriteLine($"Name: {repo.Name}, Link: {repo.Url}");
-                }
-
-                Console.ReadKey();
-
-            }).GetAwaiter().GetResult();
+            Console.ReadKey();
         }
     }
 }
